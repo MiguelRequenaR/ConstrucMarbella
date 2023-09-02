fdf8ab401729c098d83f920d8e641bbd1cea18a0
MiguelRequenaR <e_2019100844l@uncp.edu.pe>
2023-09-01 23:15:16 -0500

    create home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,20 +4,20 @@ import 'remixicon/fonts/remixicon.css'
 
 export default function Header() {
   return (
-    <div>
-        <header className='container mx-auto'>
-            <nav className='flex justify-between items-center py-6'>
-                <div>
+    <div className='fixed w-full'>
+        <header className='w-full bg-white'>
+            <nav className='container mx-auto flex justify-between items-center py-3'>
+                <div className='flex items-center'>
                     <Image src="/images/logoCM.png" alt="Logo" 
                         width={100} height={100} className='rounded-full'/>
-                    <h1></h1>
+                    <h1 className='text-gray-600 text-2xl font-semibold px-5'>Constructora Marbella</h1>
                 </div>
                 <div className='flex items-center'>
-                    <div className='space-x-10 font-semibold'>
+                    <div className='space-x-10 font-semibold text-gray-600 '>
                         <a href="">Proyectos</a>
                         <a href="">Sostenibilidad</a>
                         <a href="">Innovación</a>
-                        <i className="ri-menu-line text-2xl"></i>
+                        <i className="text-black ri-menu-line text-2xl"></i>
                     </div>
                 </div>
             </nav>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,37 @@
 import Header from "../components/Header";
-import Image from "next/image";
-
 
 export default function Home() {
   return (
-    <div>
-        <Header/>
+    <div className="">
+      <Header />
         <div>
-            <Image src="/images/fondo1.jpg" alt="" width={full} height={full}/>
+            <div
+              className="min-h-screen bg-cover bg-center w-full"
+              style={{ backgroundImage: "url('/images/carrousel1.png')" }}
+            >
+            </div>
+              <div className="container mx-auto absolute inset-0 flex items-center px-40">
+                <div className="text-left"> {/* Envuelve el texto y el botón en un div */}
+                  <h1 className="text-7xl font-bold text-white shadow-lg py-6">
+                    Construyendo <br /> tu vida
+                  </h1>
+                  <button className="text-lg mt-4 px-6 py-3 bg-blue-800 text-white rounded-full shadow-md hover:bg-blue-600 ">
+                    Conocer más
+                  </button>
+                </div>
+              </div>
+              <div className="absolute bottom-10 right-52 m-4 p-8 bg-white rounded-tl-3xl rounded-bl-3xl rounded-tr-3xl shadow-lg space-y-4">
+                <div className="flex space-x-4">
+                  <i className="ri-ruler-fill text-2xl text-blue-600"></i>
+                  <h2 className="text-lg font-semibold text-gray-600">Obras contruidas</h2>
+                </div>
+                <div className="w-full mx-auto border border-blue-400"></div>
+                <p className="text-blue-800 text-3xl font-bold">+350 ejecutadas</p>
+              </div>
+            <div>
+              asdghasdgh asdhasdh
+            </div>
         </div>
     </div>
-  )
+  );
 }
